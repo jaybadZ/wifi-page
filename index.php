226e9b97652d8e1809dc82961379d19e18576b16
Jay <bigbadaz@gmail.com>
2014-12-02 16:27:13 -0500

    Version 1
    First version of the landing page code

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>AAA Carolinas Wi-Fi Landing Page</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
+
+        <link rel="stylesheet" href="http://forms.carolinas.aaa.com/Landing-Pages/Wireless/css/style.css">
+
+        <!--[if lt IE 9]>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+
+
+        <![endif]-->
+
+        <link rel="shortcut icon" href="http://carolinas.aaa.com/PublishingImages/favicon-wifi.ico" type="image/x-icon">
+        <link rel="icon" href="http://carolinas.aaa.com/PublishingImages/favicon-wifi.ico" type="image/x-icon">
+
+
+		
+	</head><!--/head-->
+	<body data-spy="scroll">
+        <header id="header" role="banner">
+            <div class="container">
+
+                <nav class="navbar navbar-default" role="navigation">
+
+                  <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <img class="img-responsive"  alt="AAA logo" src="http://carolinas.aaa.com/Style%20Library/AAA/Images/logo_AAA.png" />
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Discover Membership<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Join AAA</a></li>
+                            <li><a href="#">Membership Levels</a></li>
+                            <li><a href="#">Benefits</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Transfer</a></li>
+                            <li><a href="#">Corporate Memberships</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">My Membership</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Find Discounts<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Shopping</a></li>
+                            <li><a href="#">Dining</a></li>
+                            <li><a href="#">Attraction</a></li>
+                            <li><a href="#">Health</a></li>
+                            <li><a href="#">Financial</a></li>
+                            <li><a href="#">Auto</a></li>
+                            <li><a href="#">Search</a></li>
+                            <li><a href="#">AAA ClubDeal</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Plan Travel<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Flights</a></li>
+                            <li><a href="#">Hotels</a></li>
+                            <li><a href="#">Cars</a></li>
+                            <li><a href="#">Cruises</a></li>
+                            <li><a href="#">Vacation Packages</a></li>
+                            <li><a href="#">Deals</a></li>
+                            <li><a href="#">Things to Do</a></li>
+                            <li><a href="#">Travel with AAA</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">1-800-444-8691</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Get Insurance<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Get a Quote</a></li>
+                            <li><a href="#">Auto</a></li>
+                            <li><a href="#">Homeowners</a></li>
+                            <li><a href="#">Motorcycle</a></li>
+                            <li><a href="#">Life & Health</a></li>
+                            <li><a href="#">Travel Insurance</a></li>
+                            <li><a href="#">Find an Agent</a></li>
+                            <li><a href="#">AAA Insurance</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Explore Car Care<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Get a Quote</a></li>
+                            <li><a href="#">Auto</a></li>
+                            <li><a href="#">Homeowners</a></li>
+                            <li><a href="#">Motorcycle</a></li>
+                            <li><a href="#">Life & Health</a></li>
+                            <li><a href="#">Travel Insurance</a></li>
+                            <li><a href="#">Find an Agent</a></li>
+                            <li><a href="#">AAA Insurance</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Buy Vehicles<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Get a Quote</a></li>
+                            <li><a href="#">Auto</a></li>
+                            <li><a href="#">Homeowners</a></li>
+                            <li><a href="#">Motorcycle</a></li>
+                            <li><a href="#">Life & Health</a></li>
+                            <li><a href="#">Travel Insurance</a></li>
+                            <li><a href="#">Find an Agent</a></li>
+                            <li><a href="#">AAA Insurance</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Drive Safely<span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Get a Quote</a></li>
+                            <li><a href="#">Auto</a></li>
+                            <li><a href="#">Homeowners</a></li>
+                            <li><a href="#">Motorcycle</a></li>
+                            <li><a href="#">Life & Health</a></li>
+                            <li><a href="#">Travel Insurance</a></li>
+                            <li><a href="#">Find an Agent</a></li>
+                            <li><a href="#">AAA Insurance</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+
+ </header>
+
+        <!--Banner Image-->
+        <section id="main-image" class="carousel">
+            <div class="layer">
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="container">
+                                <div class="carousel-content">
+
+                                    <h1>Welcome To AAA Carolinas</h1>
+                                </div>
+                            </div>
+                    </div>
+             </div>
+            </div>
+        </section>
+        <!--Banner Image End-->
+
+        <!--Content and button area -->
+        <section id="main-content">
+            <div class="container">
+                <div class="box first">
+                    <div class="row content">
+                        <p style="text-align: center; color: #494949">Membership matters and service is key whether it be in travel, insurance, car care or discounts. </p>
+                        <p style="text-align: center; color: #bf4040">Please Select To Access Wi-Fi</p>
+
+                        <div class="centerbtn">
+                            <form action="<?php $_GET['base_grant_url'] . '?continue_url=' . $_GET['user_continue_url'] ?>" method="get">
+
+                            <button class="step-button action-button finish-step" type="submit" id="login-btn" name="login-btn" value="Login To Wi-Fi">LOGIN</button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="container">
+                <p style="">© Copyright AAA Carolinas &copy; <?php echo date('Y') ?> . All Rights Reserved.</p>
+            </div>
+        </footer>
+
+
+<!--Jquery reference for all pages-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
